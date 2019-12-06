45b15031fe59d301ce05808e1fd23222dd52cdb8
Jacob Washburn <jacobrwashburn@gmail.com>
2019-12-06 10:37:57 -0700

    playersList.js: map over playersList data

---
+++ client/src/Components/PlayersList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import PlayerCard from './PlayerCard';
+
+const PlayersListWrapper = styled.div`
+  
+`;
+
+
+const PlayersList = ({players}) => {
+    return (
+        <PlayersListWrapper>
+            {players.map(player => <PlayerCard key={player.name} player={player}/>)}
+        </PlayersListWrapper>
+    );
+};
+
+export default PlayersList;
